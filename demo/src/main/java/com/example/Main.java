4365131a7b3f0b5a671f49457ec2d08647257fbf
usergoin <gustavo.roberto.silva2019@gmail.com>
2026-09-01 11:04:19 -0300

    Resolução do exercicio aula01/09

--- demo/src/main/java/com/example/Main.java
+++ demo/src/main/java/com/example/Main.java
@@ -1,19 +1,45 @@
 package com.example;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+     Scanner feel = new Scanner(System.in);
 
-        calculateSum(5, 10, "Danilo");
-        calculateSum(60, 9, "Sampaio");
-        calculateSum(30, 8, "Gabriela");
-        calculateSum(69, 67, "Heitor");
-        calculateSum(23, 20, "Luana");
+     double valores[] = new double[5];
+
+     for (int i = 0; i < 5; i++){
+
+        System.out.println("Digite um valor " + (i + 1) + " ");
+        valores[i] = feel.nextDouble();
+        }
+
+        double soma = 0;
+        double maiorvalor = valores[0];
+        double menorValor = valores[0];
+        int indiceMaior = 0;
+        int indiceMenor = 0;
+        
+        for(int i = 0; i < valores.length; i++) {
+            soma += valores[i];
+
+            if (valores[i] > maiorvalor){
+                maiorvalor = valores[i];
+                indiceMaior = i;
+            }
+
+            if(valores[i] < menorValor) {
+                menorValor = valores[i];
+                indiceMenor = i;
+            }
+        }
+
+        double media = soma / valores.length;
+
+        System.out.printf("Total do dia: %.2f%n", soma);
+        System.out.printf("Media por caixa: %.2f%n", media);
+        System.out.printf("Maior venda: Caixa %d com %.2f%n", (indiceMaior + 1), maiorvalor);
+        System.out.printf("Menor venda: Caixa %d com %.2f%n", (indiceMenor + 1), menorValor);
+  
+     }
+
     }
-
-  public static void calculateSum(int a, int b, String userName) {
-        System.out.println(a + b + " " + userName);
-    }  
-
-
-}
